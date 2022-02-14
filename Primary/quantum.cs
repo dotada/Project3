ef3872e2069f7904003df848b0b8ac7e9931a563
ANNA BLAZHEVA <luboslav.blazhev@gmail.com>
2022-02-14 10:28:29 +0200

    made it so the quantum command no longer uses a local file but a file on github

--- Primary/quantum.cs
+++ Primary/quantum.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace Primary
 {
@@ -10,9 +11,10 @@ namespace Primary
     {
         public void quantumm()
         {
-            string dir = Environment.CurrentDirectory;
-            string text = System.IO.File.ReadAllText(dir + "\\pc.txt");
-            Console.WriteLine(text);
+            HttpClient client = new HttpClient();
+            Task<string> task = client.GetStringAsync("https://raw.githubusercontent.com/dotada/Project3/master/Primary/ascii-art.txt");
+            task.Wait();
+            Console.WriteLine(task.Result);
             System.Threading.Thread.Sleep(2000);
             Console.WriteLine("Press enter to exit.");
             Console.ReadLine();
